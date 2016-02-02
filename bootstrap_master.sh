7484df4e640ef6aa79daaed19a229950dfe4db8b
Luca Francesca <luca.francesca01@gmail.com>
2016-02-02 21:29:07 +0000

    Working

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -5,5 +5,5 @@ sudo sh -c 'echo "192.168.100.21 jenkins.local jenkins" >> /etc/hosts'
 sudo sh -c 'echo "192.168.100.11 jenkinslave.local jenkinslave"  >> /etc/hosts'
 sudo apt-get autoremove -y
 sudo -u jenkins sh -c "echo -e  'y\n'| ssh-keygen -q -t rsa -N '' -f /var/lib/jenkins/.ssh/id_rsa"
-sshpass -p 'jenkins' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "jenkins@jenkinslave.local"  "echo `cat /var/lib/jenkins/.ssh/id_rsa` >> /var/lib/jenkins/.ssh/authorized_keys"
+sshpass -p 'jenkins' scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /var/lib/jenkins/.ssh/id_rsa.pub "jenkins@jenkinslave.local:/var/lib/jenkins/.ssh/authorized_keys"
 sudo service jenkins start
