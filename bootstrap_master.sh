35a60a786c432a83565ce4d2b8b4fe9aab141454
Luca Francesca <luca.francesca01@gmail.com>
2016-02-02 20:35:23 +0000

    Trying to fix ssh key problem again

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y --force-yes jenkins git sshpass
 sudo sh -c 'echo "192.168.100.21 jenkins.local jenkins" >> /etc/hosts'
 sudo sh -c 'echo "192.168.100.11 jenkinslave.local jenkinslave"  >> /etc/hosts'
 sudo apt-get autoremove -y
-sudo ssh -c "ssh-keygen -b 1024 -N '' -f /var/lib/jenkins/.ssh/id_rsa"
-sudo ssh -c 'echo "jenkins:jenkins" | chpasswd'
-cat /var/lib/jenkins/.ssh/id_rsa| sshpass -p 'jenkins' ssh "jenkins@jenkinslave.local"  "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys"
+sudo -u jenkins "echo -e  'y\n'|ssh-keygen -q -t rsa -N '' -f /var/lib/jenkins/.ssh/id_rsa"
+sshpass -p 'vagrant' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "vagrant@jenkinslave.local" 'echo "jenkins:jenkins" | chpasswd'
+cat /var/lib/jenkins/.ssh/id_rsa| sshpass -p 'jenkins' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "jenkins@jenkinslave.local"  "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys"
 sudo service jenkins start
