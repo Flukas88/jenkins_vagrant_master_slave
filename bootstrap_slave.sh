8416b3720dd9ab35becada17c8c7d704e8f867cd
Luca Francesca <Flukas88@users.noreply.github.com>
2016-02-13 15:43:45 +0000

    Added locale generation for slave

--- bootstrap_slave.sh
+++ bootstrap_slave.sh
@@ -5,6 +5,6 @@ sudo useradd -m -d /var/lib/jenkins -g jenkins jenkins
 sudo sh -c 'echo "jenkins:jenkins" | chpasswd'
 sudo mkdir -p /var/lib/jenkins/.ssh
 sudo chown -R jenkins:jenkins /var/lib/jenkins/.ssh
-sudo
 sudo apt-get install -y --force-yes openjdk-7-jre-headless git
 sudo apt-get autoremove -y
+sudo locale-gen en_US.UTF-8
